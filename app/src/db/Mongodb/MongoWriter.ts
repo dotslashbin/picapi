313a007e064b7c1e57e01862f287734c773637f7
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-11 11:28:37 +0800

    Initial implementation of writing service

--- app/src/db/Mongodb/MongoWriter.ts
+++ app/src/db/Mongodb/MongoWriter.ts
@@ -1,10 +1,10 @@
-import { PDDataPayload } from '../../structures/types'
-import { DBWriter } from '../../structures/interfaces'
+// import { DBWriter } from '../../structures/interfaces'
 import DBCore from '../DBCore'
+import { PhotoPayload } from '../../structuresRef/types'
 
 export default class MongoWriter extends DBCore implements DBWriter {
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	Save(params: PDDataPayload, model: any): any {
+	Save(params: PhotoPayload, model: any): any {
 		try {
 			return model.create(params)
 		} catch (error) {

019dc4bb8d5bb7307936afd36ee89d0d60353c54
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-11 23:16:27 +0800

    Added wrapper for proper return

--- app/src/helpers/Utilities.ts
+++ app/src/helpers/Utilities.ts
@@ -1,3 +1,27 @@
+/**
+ * Returns the formatted object for a photo data
+ * @param id
+ * @param description
+ * @param available
+ * @returns
+ */
+export function getCleanedDataReturn(
+	id: string,
+	description: string,
+	available: boolean
+): { id: string; description: string; available: boolean } {
+	return {
+		id,
+		description,
+		available,
+	}
+}
+
+/**
+ * Returns a formatted object for an error
+ * @param error
+ * @returns
+ */
 export function getErrorReturn(error: any): { failed: boolean; error: any } {
 	return {
 		failed: true,
